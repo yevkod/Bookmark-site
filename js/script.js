55601be10bd8700defe3e70b063b642256a4e9b8
Yevgen <evgennevidim@gmail.com>
2023-05-08 21:58:44 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const tabs = document.querySelectorAll('.tab')
+const panels = document.querySelectorAll('.panel')
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+const logo = document.getElementById('logo')
+
+
+tabs.forEach((tab) => tab.addEventListener('click', onTabClick))
+
+
+btn.addEventListener('click', navToggle)
+
+
+
+function onTabClick(e) {
+
+  tabs.forEach((tab) => {
+    tab.children[0].classList.remove(
+      'border-softRed',
+      'border-b-4',
+      'md:border-b-0'
+    )
+  })
+
+
+  panels.forEach((panel) => panel.classList.add('hidden'))
+
+
+  e.target.classList.add('border-softRed', 'border-b-4')
+  const classString = e.target.getAttribute('data-target')
+  document
+    .getElementById('panels')
+    .getElementsByClassName(classString)[0]
+    .classList.remove('hidden')
+}
+
+function navToggle() {
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+
+  if (menu.classList.contains('flex')) {
+    logo.setAttribute('src', './images/logo-bookmark-footer.svg')
+  } else {
+    logo.setAttribute('src', './images/logo-bookmark.svg')
+  }
+}
